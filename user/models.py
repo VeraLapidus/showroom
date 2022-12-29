f7ee28f41ea523c1f302ae9e69e5c6c281484d5b
VeraLapidus <lapidus.vera@gmail.com>
2022-12-29 15:23:44 +0300

    app User - added choices to models.py; deleted enums.py

--- user/models.py
+++ user/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from user.enums import Usertype
-
 
 class User(AbstractUser):
+    USERTYPE = (
+        ('AutoShow', 'AutoShow'),
+        ('Customer', 'Customer'),
+        ('Producer', 'Producer'),
+        ('Admin', 'Admin'),
+    )
+    usertype = models.CharField(max_length=50, choices=USERTYPE)
     email = models.EmailField(max_length=255, unique=True)
-    usertype = models.CharField(max_length=50, choices=Usertype.choices())
 
     REQUIRED_FIELDS = ['usertype', 'email']
 
