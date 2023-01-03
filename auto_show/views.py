7dbf07b340386beb1f722d349f14a4d0dff3761c
VeraLapidus <lapidus.vera@gmail.com>
2023-01-03 16:41:47 +0300

    statistics for auto_shows and producers

--- auto_show/views.py
+++ auto_show/views.py
@@ -1,13 +1,17 @@
-from django.core.mail import send_mail
+from django.db.models import Q, Sum
 
 from django_filters import rest_framework as filters
 from rest_framework import mixins
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from auto_show.filters import AutoShowFilter
 from auto_show.models import AutoShow
 from auto_show.serializers import AutoShowSerializer, AutoShowSerializerCreate
+from deals.models import Deal
+from deals.serializers import DealSerializer
 
 
 class AutoShowViewSet(mixins.CreateModelMixin,
@@ -21,12 +25,24 @@ class AutoShowViewSet(mixins.CreateModelMixin,
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = AutoShowFilter
 
-    # ordering_fields = ['year_foundation']
+    @action(methods=['get'], detail=True)
+    def statistics(self, request, pk=None):
+        auto_show = AutoShow.objects.get(pk=pk)
+        queryset_for_statistics = Deal.objects.filter(Q(participants="Producer-AutoShow") & Q(auto_shows=auto_show.id))
+        amount_of_bought_cars = queryset_for_statistics.count()
+        consumption = queryset_for_statistics.aggregate(Sum('price'))['price__sum']
+
+        # bought_cars
+        bought_cars = []
+        for deal in DealSerializer(queryset_for_statistics, many=True).data:
+            bought_cars.append(deal.get("car_instances"))
+
+        context = {'auto_show': auto_show.name, 'amount_of_bought_cars': amount_of_bought_cars,
+                   'consumption, USD': consumption, 'bought_cars': bought_cars}
+        return Response(context)
 
     def get_serializer_class(self, *args, **kwargs):
         if self.request.method == "POST":
             return AutoShowSerializerCreate
 
         return self.serializer_class
-
-
--- producer/views.py
+++ producer/views.py
@@ -1,7 +1,12 @@
+from django.db.models import Q, Sum
 from django_filters import rest_framework as filters
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
+from deals.models import Deal
+from deals.serializers import DealSerializer
 from producer.filters import ProducerFilter
 from producer.models import Producer
 from producer.serializers import ProducerSerializer, ProducerSerializerCreate
@@ -13,6 +18,24 @@ class ProducerViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets
     permission_classes = (IsAuthenticatedOrReadOnly,)
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ProducerFilter
+
+    @action(methods=['get'], detail=True)
+    def statistics(self, request, pk=None):
+        producer = Producer.objects.get(pk=pk)
+        queryset_for_statistics = Deal.objects.filter(Q(participants="Producer-AutoShow") & Q(producers=producer.id))
+        amount_of_sold_cars = queryset_for_statistics.count()
+        profit = queryset_for_statistics.aggregate(Sum('price'))['price__sum']
+
+        # name and amount of unique clients
+        unique_clients_list = []
+        for deal in DealSerializer(queryset_for_statistics, many=True).data:
+            unique_clients_list.append(deal.get("auto_shows"))
+        unique_clients_set = set(unique_clients_list)
+        amount_of_unique_clients = len(unique_clients_set)
+
+        context = {'producer_name': producer.name, 'amount_of_sold_cars': amount_of_sold_cars, 'profit': profit,
+                   'unique_clients': unique_clients_set, 'amount_of_unique_clients': amount_of_unique_clients}
+        return Response(context)
 
 
 def get_serializer_class(self, *args, **kwargs):
