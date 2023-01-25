1dad4e36a02c0f1c2e96e779881afe8e9900b5d8
VeraLapidus <lapidus.vera@gmail.com>
2023-01-25 17:22:38 +0300

    added testing response.data to statistic

--- auto_show/tests.py
+++ auto_show/tests.py
@@ -13,26 +13,21 @@ def api_client():
 
 
 @pytest.fixture
-def create_user() -> User:
+def create_user_auto_show() -> User:
     return User.objects.create(
-        username="Ford Minsk",
+        username="Ford AutoShow",
         password="12345",
         usertype="AutoShow",
         email="trainee5@gmail.com")
 
 
 @pytest.fixture
-def create_auto_show(create_user) -> AutoShow:
+def create_auto_show(create_user_auto_show) -> AutoShow:
     return AutoShow.objects.create(
-        name="Ford",
+        name="Ford Minsk",
         country="DE",
         balance=200.00,
-        owner=User.objects.get(username="Ford Minsk"))
-
-
-@pytest.mark.django_db
-def test_user_create(create_user):
-    assert User.objects.filter(username="Ford Minsk").exists()
+        owner=User.objects.get(username="Ford AutoShow"))
 
 
 @pytest.mark.django_db
@@ -58,7 +53,7 @@ def test_auto_show_response_data(create_auto_show, api_client):
 
 @pytest.mark.django_db
 def test_auto_show_delete(create_auto_show):
-    AutoShow.objects.filter(name="Ford").delete()
+    AutoShow.objects.filter(name="Ford Minsk").delete()
     assert AutoShow.objects.count() == 0
 
 
@@ -66,4 +61,11 @@ def test_auto_show_delete(create_auto_show):
 def test_auto_show_statistic(create_auto_show, api_client):
     url = reverse('autoshow-stat', kwargs={'pk': create_auto_show.id})
     response = api_client.get(url, pk=create_auto_show.id)
+    expected_data = {
+        "auto_show": "Ford Minsk",
+        "amount_of_bought_cars": 0,
+        "consumption, USD": None,
+        "bought_cars": []
+    }
     assert response.status_code == 200
+    assert response.data == expected_data
--- producer/tests.py
+++ producer/tests.py
@@ -1,7 +1,12 @@
+from decimal import Decimal
+
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
 
+from auto_show.models import AutoShow
+from car.models import Car, CarInstance
+from deals.models import Deal
 from producer.models import Producer
 from user.models import User
 
@@ -12,27 +17,67 @@ def api_client():
 
 
 @pytest.fixture
-def create_user() -> User:
+def create_user_producer() -> User:
     return User.objects.create(
-        username="Mercedes Minsk",
+        username="Mercedes Producer",
         password="12345",
         usertype="Producer",
         email="trainee7@gmail.com")
 
 
 @pytest.fixture
-def create_producer(create_user) -> Producer:
+def create_user_auto_show() -> User:
+    return User.objects.create(
+        username="Mercedes AutoShow",
+        password="12345",
+        usertype="AutoShow",
+        email="trainee5@gmail.com")
+
+
+@pytest.fixture
+def create_producer(create_user_producer) -> Producer:
     return Producer.objects.create(
-        name="Mercedes",
+        name="Mercedes Berlin",
         country="DE",
         balance=100.00,
-        owner=User.objects.get(username="Mercedes Minsk"))
+        owner=User.objects.get(username="Mercedes Producer"))
 
 
-@pytest.mark.statistic
-@pytest.mark.django_db
-def test_producer(create_producer):
-    assert Producer.objects.count() == 1
+@pytest.fixture
+def create_auto_show(create_user_auto_show) -> AutoShow:
+    return AutoShow.objects.create(
+        name="Mercedes Minsk",
+        country="DE",
+        balance=200.00,
+        owner=User.objects.get(username="Mercedes AutoShow"))
+
+
+@pytest.fixture
+def create_car() -> Car:
+    return Car.objects.create(
+        brand="Mercedes",
+        model="GL",
+        year=2022)
+
+
+@pytest.fixture
+def create_car_instance(create_car, create_auto_show) -> CarInstance:
+    return CarInstance.objects.create(
+        name=Car.objects.get(brand="Mercedes"),
+        condition="At AutoShow",
+        price=80.00,
+        auto_shows=AutoShow.objects.get(name="Mercedes Minsk"))
+
+
+@pytest.fixture
+def create_deal(create_producer, create_auto_show, create_car_instance) -> Deal:
+    return Deal.objects.create(
+        name='Mercedes GL 2022 from Mercedes Berlin to Mercedes Minsk',
+        participants="Producer-AutoShow",
+        producers=Producer.objects.get(name="Mercedes Berlin"),
+        auto_shows=AutoShow.objects.get(name="Mercedes Minsk"),
+        car_instances=CarInstance.objects.get(condition="At AutoShow"),
+        price=80.00)
 
 
 @pytest.mark.statistic
@@ -43,9 +88,24 @@ def test_producer_api(api_client):
     assert response.status_code == 200
 
 
+@pytest.mark.django_db
+def test_deal_create(create_deal):
+    assert Deal.objects.count() == 1
+
+
 @pytest.mark.statistic
 @pytest.mark.django_db
-def test_producer_statistic(create_producer, api_client):
+def test_producer_statistic(create_producer, create_deal, api_client):
     url = reverse('producer-stat', kwargs={'pk': create_producer.id})
     response = api_client.get(url, pk=create_producer.id)
+    expected_data = {
+        "producer_name": "Mercedes Berlin",
+        "amount_of_sold_cars": 1,
+        "profit": Decimal('80.00'),
+        "unique_clients": {
+            "Mercedes Minsk"
+        },
+        "amount_of_unique_clients": 1
+    }
     assert response.status_code == 200
+    assert response.data == expected_data
